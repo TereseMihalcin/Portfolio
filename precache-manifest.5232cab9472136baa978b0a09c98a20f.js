02627753bce9aaad3e72e6c371edaafcd26d6af7
TereseMihalcin <teresemihalcin18@gmail.com>
2022-04-14 23:21:24 -0400

    Updates

---
+++ precache-manifest.5232cab9472136baa978b0a09c98a20f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae03fae8abdfee287503318499c0aca9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77d53c74d86391e69773",
+    "url": "/static/css/2.e45429e2.chunk.css"
+  },
+  {
+    "revision": "49b74abc5cdd19e2142f",
+    "url": "/static/css/main.167cd92c.chunk.css"
+  },
+  {
+    "revision": "77d53c74d86391e69773",
+    "url": "/static/js/2.963360aa.chunk.js"
+  },
+  {
+    "revision": "923a93445da4455d8e7e236ec4fa5442",
+    "url": "/static/js/2.963360aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49b74abc5cdd19e2142f",
+    "url": "/static/js/main.48da04cb.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
