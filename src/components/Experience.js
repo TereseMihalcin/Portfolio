5cb2e6f36d91afb47d4a20e838891160b59250de
TereseMihalcin <teresemihalcin18@gmail.com>
2022-04-18 16:47:51 -0400

    add my video

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -22,7 +22,7 @@ class Video extends Component {
                 <ReactPlayer 
                 style={{ margin: "30px" }}
                 controls
-                url = 'https://www.youtube.com/watch?v=UlbJXGSf9TM'/>
+                url = 'https://youtu.be/fZkEyPKB1eo'/>
               </div>
             </div>
           </div>
