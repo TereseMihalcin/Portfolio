5fc4024d138eb909824816d738674ced1083a0ff
Terese Mihalcin <teresemihalcin18@gmail.com>
2024-12-07 22:48:22 -0500

    update experience and mobile view

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -13,12 +13,13 @@ class Experience extends Component {
           <h1 class="section-title" style={{ color: "black" }}>
             <span>{sectionName}</span>
             </h1>
-          <div className="row mx-auto mb-5 col-6">
-            <div className="col">
+          <div className="row mx-auto mb-5 col-8">
+            <div className="col-md-6 mb-5">
               <h2 class="company">Creative Circle Media Solutions</h2>
-              <h3 class="year">2022-Ongoing</h3>
+              <h3 class="job-title">Software Developer</h3>
+              <h5 class="year">2022-Ongoing</h5>
             </div>
-              <div className="col">
+              <div className="col-md-6">
                 <div className="card" style={{background: "transparent"}}>
                     <div className="card-header" style={{background: "#e1cfa2"}}>
                       <span
@@ -35,7 +36,7 @@ class Experience extends Component {
                     lineHeight: "200%",
                   }}
                   >
-                    I've spent over the last two years working at Creative Circle. This was my first experience at a "real" job post college. Here I use php and SQL for backend / data management and Javascript, HTML, CSS, Bootstrap for frontend display. I spend my time building numerous websites and making improvments to our "core" software repo which supports our web CMS and numerous "Apps" used in our websites.   
+                    I've spent over the last two years working as a developer on the Creative Circle team. Here I use php and MySQL for backend / data management and Javascript, HTML, CSS, Bootstrap for frontend display. I spend my time building numerous websites and making improvments to our core software repo which supports our web CMS and numerous apps used in our websites.   
                   </div>
                 </div>
               </div>
